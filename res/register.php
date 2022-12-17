6e0605a1b2b9aee782eebbd7e25075065b8eafab
Anggito02 <manaluanggito@gmail.com>
2022-12-18 00:44:59 +0700

    Revisi desain

--- res/register.php
+++ res/register.php
@@ -69,6 +69,7 @@
             <input type="file" class="form-control" id="foto"name="foto" aria-describedby="fotoHelp" required>
         </div>
         <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+        <a href="../index.php"><button type="button" class="my-2 btn border border-primary text-primary">Back To Index</button></a>
     </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
