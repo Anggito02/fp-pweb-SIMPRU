210199bea4c7db848bdd732838e6956853ea36ce
Anggito02 <manaluanggito@gmail.com>
2022-12-20 07:42:03 +0700

    fix buat_pesanan_ruangan.php navbar

--- res/buat_pesanan_ruangan.php
+++ res/buat_pesanan_ruangan.php
@@ -36,7 +36,7 @@ $nama_ruangan = $_GET['ruangan'];
     <nav class="container-fluid navbar navbar-expand-lg bg-light sticky-top px-5">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img id="logo-simpru" src="../src/img/SimpruLOGO.png" alt="Logo SIMPRU" width="280" height="73.04" />
+                <img id="logo-simpru" src="../src/img/SimpruLOGO.png" alt="Logo SIMPRU" width="280" height="73.04">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -46,40 +46,52 @@ $nama_ruangan = $_GET['ruangan'];
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="/index.html">Beranda</a>
+                        <a class="nav-link" href="../index.php">Beranda</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/app/html/aboutus.html">Tentang Kami</a>
+                        <a class="nav-link" href="../res/about.php">Tentang Kami</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" aria-current="page" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false" aria-current="page" href="#">
                             <b>Kategori Pemesanan - Ruangan</b>
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Ruangan</a></li>
-                            <li><a class="dropdown-item" href="/app/html/kendaraan.html">Kendaraan</a></li>
-                            <li><a class="dropdown-item" href="#">Lainnya</a></li>
+                            <li><a class="dropdown-item" href="../res/list_ruangan.php">Ruangan</a></li>
+                            <li><a class="dropdown-item" href="../res/list_kendaraan.php">Kendaraan</a></li>
                         </ul>
                     </li>
                     <li class="nav-item d-md-none">
-                        <a class="nav-link" href="/app/html/profile.html">Akun Saya</a>
+                        <a class="nav-link" href="#">Akun Saya</a>
                     </li>
                 </ul>
                 <div class="d-flex">
                     <form class="d-flex mx-2" role="search">
                         <input class="form-control me-2" type="search" placeholder="Ingin menyewa apa?"
-                            aria-label="Search" />
+                            aria-label="Search">
                         <button class="btn btn-outline-success" type="submit">Cari</button>
                     </form>
                     <div class="dropdown mx-2 d-none d-md-block">
                         <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="../src/icon/AccountIcon.svg" alt="Account Icon" width="36px" height="36px" />
+                            <img src="../src/icon/AccountIcon.svg" alt="Account Icon" width="36px" height="36px">
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="/app/html/profile.html">Akun Saya</a></li>
-                            <li><a class="dropdown-item" href="#">Pesanan Saya</a></li>
-                            <li><a class="dropdown-item" href="#">Keluar</a></li>
+                            <?php if( isset($_SESSION['username'])): ?>
+                            <li class="dropdown-item">Halo, <?= $_SESSION['username'] ?></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Pemesanan Anda</a></li>
+                            <form class="my-3" action="./helper/logout_process.php" method="post">
+                                <button type="submit" class="dropdown-item">Keluar</button>
+                            </form>
+                            <?php else: ?>
+                            <li><a class="dropdown-item" href="./res/login.php">Login</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="./res/register.php">Buat Akun Baru</a></li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
